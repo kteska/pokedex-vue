e51edb1ec8e8e4db13dd6b0481253c216f716df8
kteska <kasia@teska.pl>
2020-04-16 16:17:02 +0200

    Correct docs and deleted unnecessary comment code

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
 <template>
   <v-app>
-    <!--
-    <v-toolbar color="error">
-      <v-spacer></v-spacer>
-      <v-toolbar-title class="white--text"> <div id="title">Pokedex</div></v-toolbar-title>
-      <v-spacer></v-spacer>
-    </v-toolbar>
-    -->
-
     <v-content>
       <router-view></router-view>
     </v-content>
@@ -15,13 +7,11 @@
 </template>
 
 <script>
-//import Main from "./pages/Main";
 
 export default {
   name: "App",
 
   components: {
-    //Main,
   },
 
   data: () => ({
@@ -32,15 +22,7 @@ export default {
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css?family=Acme|Fredoka+One|Fugaz+One|Gochi+Hand|Lexend+Giga|Monoton|Press+Start+2P&display=swap");
-/**
-font-family: 'Acme', sans-serif;
-font-family: 'Gochi Hand', cursive;
-font-family: 'Fredoka One', cursive; +
-font-family: 'Lexend Giga', sans-serif;
-font-family: 'Monoton', cursive;
-font-family: 'Press Start 2P', cursive; ++
-font-family: 'Fugaz One', cursive;
- */
+
 #title {
   color: white;
   font-size: 24px;
